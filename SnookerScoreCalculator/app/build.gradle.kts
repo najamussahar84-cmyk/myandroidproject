4ff3bda0556672ff831f4a54f17a119f94df6cf9
najamussahar84-cmyk <najamussahar84@gmail.com>
2026-08-28 04:14:20 +0500

    newsss

--- SnookerScoreCalculator/app/build.gradle.kts
+++ SnookerScoreCalculator/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.snooker.scorecalculator"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.snooker.scorecalculator"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
